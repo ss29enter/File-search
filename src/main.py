017998853c710b4c1ead7a8ffb393a8fc299bb94
kss <curiosizee@gmail.com>
2026-07-19 12:11:15 +0300

    NEW FEATURES
    Added a simple search:
    - by extension;
    - exactly by size in bytes

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ from searcher import search_file
 
 
 base_dir = input('Seacrh path: ')
-pattern = input('File name: ')
+pattern = input('File name: '), input('Extension: '), int(input('Size (bytes): '))
 
 def main():
     files = search_file(base_dir, pattern)
 
-    print('> Found: {} files\n'.format(len(files)))
-    for id, file in enumerate(files, 1):
+    print('\n> Found: {} files'.format(len(files)))
+    for id, file, in enumerate(files, 1):
         print(f'{id}. {file}')
 
 
--- src/searcher.py
+++ src/searcher.py
@@ -2,11 +2,26 @@ from pathlib import Path
 
 
 def search_file(directory, pattern):
+    name, ext, size = pattern
+    
     directory = Path(directory)
     result = []
     
     for path in directory.rglob('*'):
-        if pattern.lower() in path.name.lower():
+        if (
+            match_name(path, name)
+            and match_extension(path, ext)
+            and match_size(path, size)
+        ):
             result.append(path)
 
     return sorted(result)
+
+def match_name(file, name):
+    return name.lower() in file.name.lower()
+
+def match_extension(file, ext):
+    return ext == file.suffix
+
+def match_size(file, size):
+    return file.stat().st_size == size
