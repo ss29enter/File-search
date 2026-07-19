95b0537dd9809f4341cafa34a684fb5d2fb18cf1
kss <curiosizee@gmail.com>
2026-07-19 13:26:51 +0300

    NEW FEATURES
    - Search by file size in B,KB,MB...
    - The option to search for sizes larger/smaller than this one

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ from searcher import search_file
 
 
 base_dir = input('Seacrh path: ')
-pattern = input('File name: '), input('Extension: '), int(input('Size (bytes): '))
+pattern = input('File name: '), input('Extension: [ exp: .py ] '), input('Size: [ exp: > 10 KB ] ').split()
 
 def main():
     files = search_file(base_dir, pattern)
 
     print('\n> Found: {} files'.format(len(files)))
-    for id, file, in enumerate(files, 1):
+    for id, file in enumerate(files, 1):
         print(f'{id}. {file}')
 
 
--- src/searcher.py
+++ src/searcher.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
-
 def search_file(directory, pattern):
     name, ext, size = pattern
-    
     directory = Path(directory)
     result = []
     
@@ -24,4 +22,31 @@ def match_extension(file, ext):
     return ext == file.suffix
 
 def match_size(file, size):
-    return file.stat().st_size == size
+    operation, *size_unit = size
+    bytes = format_size(size_unit)
+    file_size = file.stat().st_size
+    print(bytes, file_size)
+    if operation == '=':
+        return bytes == file_size
+    
+    elif operation == '>':
+        return file_size > bytes
+
+    elif operation == '<':
+        return file_size < bytes
+
+def format_size(size):
+    size_int, unit = size
+    size_int = int(size_int)
+    if size_int == 0:
+        return 0
+    elif unit == 'B':
+        return size_int
+    elif unit == 'KB':
+        return size_int*2**10
+    elif unit == 'MB':
+        return size_int*2**20
+    else: 
+        return size_int*2**30
+    
+
